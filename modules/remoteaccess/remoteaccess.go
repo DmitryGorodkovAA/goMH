e2993af132c1b55caa13363fa77e624f5b10b252
DmitryGorodkov <d.gorodkov@gwd.ru>
2025-11-28 23:31:48 +0300

    fix

--- modules/remoteaccess/remoteaccess.go
+++ modules/remoteaccess/remoteaccess.go
@@ -40,7 +40,7 @@ func (m *Module) Run(am core.AssetManager, wu core.WinUtils) error {
 		{ID: "2", Name: "LiteManager", ServiceName: "ROMService", InstallFunc: m.installLiteManager, AllowReinstall: false},
 		{ID: "3", Name: "Getad Agent", ServiceName: "MH_Getad", InstallFunc: m.installGetad, AllowReinstall: true},
 		{ID: "4", Name: "AnyDesk", ServiceName: "AnyDesk", InstallFunc: m.installAnyDesk, AllowReinstall: false},
-		{ID: "5", Name: "Aspia Client", ServiceName: "AspiaClient", InstallFunc: m.installAspiaClient, AllowReinstall: false},
+		{ID: "5", Name: "Aspia Client", ServiceName: "aspia-host-service", InstallFunc: m.installAspiaClient, AllowReinstall: false},
 		{ID: "6", Name: "Ammy Admin", ServiceName: "AmmyAdmin", InstallFunc: m.installAmmyAdmin, AllowReinstall: false},
 	}
 
